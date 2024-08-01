fc9351eacfb063f0472419b2e203feb4dc815253
shil-mil <shyshy122303@gmail.com>
2024-08-01 14:55:50 +0800

    create function playRound

--- myScript.js
+++ myScript.js
@@ -5,18 +5,52 @@ function getComputerChoice() {
     let num = Math.random();
     
     if (num <= 0.333) {
-        return "Rock";
+        return "rock";
     } else if (num > 0.333 && num <= 0.666) {
-        return "Paper";
+        return "paper";
     }  
-        return "Scissors";
+        return "scissors";
 }
 
 function getHumanChoice() {
     let choice = prompt("Choose: rock, paper, or scissors?");
-    if(isNaN(choice || choice == "" || choice == " " || choice == null)) {
+    let c = choice.toLowerCase();
+    if(c !== "rock" || c !== "paper" || c !== "scissors" || choice == "" || choice == " " || choice == null) {
+        console.log("Invalid input. Try again!")
         getHumanChoice();
     }
-    return choice;
+    return c;
 }
 
+function playRound(humanChoice, computerChoice) {
+    if (humanChoice == "rock" && computerChoice == "rock" || humanChoice == "paper" && computerChoice == "paper" || humanChoice == "scissors" && computerChoice == "scissors") {
+        console.log("You chose the same thing!");
+    } else if (humanChoice == "rock") {
+        if (computerChoice == "paper") {
+            console.log("You lose! Paper beats rock!");
+            computerScore++;
+        } else {
+            console.log("You win! Rock beats scissors!");
+            humanScore++;
+        } 
+    } else if (humanChoice == "paper") {
+        if (computerChoice =="rock") {
+            console.log("You win! Rock beats paper!");
+            humanScore++;
+        } else {
+            console.log("You lose! Scissors beats paper!");
+            computerScore++;
+        }
+    } else if (humanChoice == "scissors") {
+        if (computerChoice == "paper") {
+            console.log("You win! Scissors beats paper!");
+            humanScore++;
+        } else {
+            console.log("You lose! Rock beats scissors!");
+            computerScore++;
+        }
+    }
+}
+
+const humanSelection = getHumanChoice();
+const computerSelection = getComputerChoice();
