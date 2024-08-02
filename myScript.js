a64e2244ffc5bc8c6af089d8f80f46708f64bfd1
shil-mil <shyshy122303@gmail.com>
2024-08-02 15:04:06 +0800

    Finalized getHumanChoice function

--- myScript.js
+++ myScript.js
@@ -14,43 +14,18 @@ function getComputerChoice() {
 
 function getHumanChoice() {
     let choice = prompt("Choose: rock, paper, or scissors?");
+    if (choice == null) {
+        console.log("You can't escape.");
+        return getHumanChoice();
+    }
+    
     let c = choice.toLowerCase();
-    if(c !== "rock" || c !== "paper" || c !== "scissors" || choice == "" || choice == " " || choice == null) {
-        console.log("Invalid input. Try again!")
-        getHumanChoice();
-    }
-    return c;
-}
-
-function playRound(humanChoice, computerChoice) {
-    if (humanChoice == "rock" && computerChoice == "rock" || humanChoice == "paper" && computerChoice == "paper" || humanChoice == "scissors" && computerChoice == "scissors") {
-        console.log("You chose the same thing!");
-    } else if (humanChoice == "rock") {
-        if (computerChoice == "paper") {
-            console.log("You lose! Paper beats rock!");
-            computerScore++;
-        } else {
-            console.log("You win! Rock beats scissors!");
-            humanScore++;
-        } 
-    } else if (humanChoice == "paper") {
-        if (computerChoice =="rock") {
-            console.log("You win! Rock beats paper!");
-            humanScore++;
-        } else {
-            console.log("You lose! Scissors beats paper!");
-            computerScore++;
-        }
-    } else if (humanChoice == "scissors") {
-        if (computerChoice == "paper") {
-            console.log("You win! Scissors beats paper!");
-            humanScore++;
-        } else {
-            console.log("You lose! Rock beats scissors!");
-            computerScore++;
-        }
+    if (c === "rock" || c === "paper" || c === "scissors") {
+        return c;
+    } else {
+        console.log("Invalid input. Try again!");
+        return getHumanChoice();
     }
 }
 
-const humanSelection = getHumanChoice();
-const computerSelection = getComputerChoice();
+
